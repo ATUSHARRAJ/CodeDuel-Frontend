6dbf7ae2eeb30ffca292bb49bae4992a27a4e036
Tushar Raj <tusharraj053@gmail.com>
2025-12-11 13:33:29 +0530

    Fixed Google Login network error

--- src/startMatch/socket.js
+++ src/startMatch/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
 
 const socket = io(SOCKET_URL, {
   transports: ['polling', 'websocket'],
